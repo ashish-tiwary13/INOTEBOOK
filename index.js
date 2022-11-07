52db5c9a813a406d9a14a21862fd0d102a00ff71
Ashish Tiwary <ashishtiwary1326@gmail.com>
2022-11-07 18:32:07 +0530

    changes done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ connectTOMongo();
 
 const express = require('express')
 const app = express()
-const port = 5000 || process.env.PORT
+const port = 5000
+//  || process.env.PORT
 //  cors policy
 var cors = require('cors')
 app.use(cors())
@@ -19,13 +20,13 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/note', require('./routes/note'))
 
-if(process.env.NODE_ENV== "production"){
-  app.use(express.static("frontend/build"));
-  const path = require("path");
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-  })
-}
+// if(process.env.NODE_ENV== "production"){
+//   app.use(express.static("frontend/build"));
+//   const path = require("path");
+//   app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+//   })
+// }
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
