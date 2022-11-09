f16f715361c2be6201251f257c84fcd43cc0b63c
ashish-tiwary13 <116632152+ashish-tiwary13@users.noreply.github.com>
2022-11-09 23:39:16 +0530

    done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/api/note', require('./routes/note'))
 __dirname = path.resolve();
 if(process.env.NODE_ENV ==='production'){
   app.use(express.static(path.join(__dirname,"/frontend/build")));
-  app.get('*',()=>{
+  app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'frontend','build',"index.html"));
   });
 }else{
