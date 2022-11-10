cb2cab7e1916e142fc97f6134e517ff92b91ce7e
ashish-tiwary13 <116632152+ashish-tiwary13@users.noreply.github.com>
2022-11-11 03:11:09 +0530

    done

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const {MOGOURI} = require('./config/keys')
+// const {MOGOURI} = require('./config/keys')
+const MOGOURI="mongodb+srv://ashishtiwary1326:Ashish%401326@cluster0.7dvvz93.mongodb.net/inotebook";
 
 const connectTOMongo = ()=> {
     mongoose.connect(MOGOURI,()=>{
